53f3274892a6c363889a414b03a87c8c1116475c
Pai314 <pai314.eequoalmc2@gmail.com>
2026-06-02 09:58:32 +0900

    model source update: d6c830f62c9b

--- src/model/CNN/constants.py
+++ src/model/CNN/constants.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class ModelConstants:
-    NUM_EPOCHS: int = 300
+    NUM_EPOCHS: int = 100
     BATCHSIZE: int = 256
-    L_LATE: float = 3 * 1e-3
-    WEIGHT_DECAY: float = 1e-4
+    L_LATE: float = 3 * 1e-4
+    WEIGHT_DECAY: float = 5e-5
 
 
 MODEL_CONSTANTS = ModelConstants()
