430f3f5b66c8fd600ea3ad7445fbbdd344db77ea
Pai314 <pai314.eequoalmc2@gmail.com>
2026-06-02 20:17:33 +0900

    model source update: df4072f299dc

--- src/model/CNN-v2/constants.py
+++ src/model/CNN-v2/constants.py
@@ -6,7 +6,7 @@ class ModelConstants:
     NUM_EPOCHS: int = 120
     BATCHSIZE: int = 256
     L_LATE: float = 3e-3
-    WEIGHT_DECAY: float = 3e-5
+    WEIGHT_DECAY: float = 1e-4
     SCHEDULER_NAME: str = "CosineAnnealingLR"
     ETA_MIN: float = 1e-5
 
