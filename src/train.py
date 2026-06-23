7fccea8341f4a52b1de8215fa61c232baa801b2e
Pai314 <293707758+suzukihinata-dev@users.noreply.github.com>
2026-06-23 14:07:31 +0900

    add label smoothing

--- src/train.py
+++ src/train.py
@@ -38,7 +38,7 @@ def main() -> None:
 
     model = build_model(meta_constants.MODEL).to(device)
 
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
     optimizer = optim.Adam(
         model.parameters(),
         lr=learning_rate,
